772c7503c7ef385bf820128157b7dd6976ef6a05
osvaldo olmos <oolmos@xsidesolutions.com>
2018-10-02 21:52:32 -0300

    commit1

---
+++ AspNetCoreTodo/AspNetCoreTodo/Services/ITodoItemService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsync(ApplicationUser currentUser);
+        Task<bool> AddItemAsync(TodoItem newItem);
+        Task<bool> MarkDoneAsync(Guid id, ApplicationUser currentUser);
+    }
+}
