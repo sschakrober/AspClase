f6f3eda2266c43666b9e65cfc1e15b29bcd2c180
osvaldo olmos <oolmos@xsidesolutions.com>
2018-10-09 19:51:13 -0300

    commit6

--- AspNetCoreTodo/AspNetCoreTodo/Controllers/ManageUsersController.cs
+++ AspNetCoreTodo/AspNetCoreTodo/Controllers/ManageUsersController.cs
@@ -6,31 +6,34 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using AspNetCoreTodo.Models;
 using Microsoft.EntityFrameworkCore;
+
 namespace AspNetCoreTodo.Controllers
 {
   [Authorize(Roles = "Administrator")]
   public class ManageUsersController : Controller
-{
-  private readonly UserManager<ApplicationUser>
-  _userManager;
- public ManageUsersController(
- UserManager<ApplicationUser> userManager)
- {
-  _userManager = userManager;
- }
- public async Task<IActionResult> Index()
-{
-  var admins = (await _userManager
-  .GetUsersInRoleAsync("Administrator"))
-  .ToArray();
-var everyone = await _userManager.Users
-.ToArrayAsync();
-var model = new ManageUsersViewModel
-{
-Administrators = admins,
-Everyone = everyone
-};
-return View(model);
+  {
+    private readonly UserManager<ApplicationUser>
+    _userManager;
+    public ManageUsersController(
+    UserManager<ApplicationUser> userManager)
+    {
+      _userManager = userManager;
+    }
+    public async Task<IActionResult> Index()
+    {
+      var admins = (await _userManager
+      .GetUsersInRoleAsync("Administrator"))
+      .ToArray();
+      var everyone = await _userManager.Users
+      .ToArrayAsync();
+      var model = new ManageUsersViewModel
+      {
+
+        Administrators = admins,
+        Everyone = everyone
+      };
+      return View(model);
+    }
+  }
 }
-}
-}
+
